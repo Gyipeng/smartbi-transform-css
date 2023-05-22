a8f9dae075f382f1f277c7acba6e607f77e5608b
gyp <826256407@qq.com>
2023-05-22 09:19:32 +0800

    新增转换css插件

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const postcss = require('postcss')
+const { replaceStr } = require('../src/utils')
+
+module.exports = postcss.plugin('smartbi-transform-css', function (opts = {}) {
+  const { prefix = 'el-', replace = 'ka-' } = opts || {}
+  
+  function plugin(css, result) {
+    css.walkRules((rule) => {
+      // 遍历当前ast所有rule节点
+      const { selector } = rule
+      if (
+        selector.includes(prefix) &&
+        !selector.includes(replace)
+      ) {
+        const clone = rule.clone()
+        clone.selector = replaceStr(selector, prefix, replace)
+        rule.replaceWith(clone)
+      }
+    })
+  }
+
+  return plugin
+})
